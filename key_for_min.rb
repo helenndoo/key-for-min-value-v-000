c5b27512e1d1135aef4308a40b5702df6ab9cf4c
Helen Du <helendu.614@gmail.com>
2018-01-09 00:39:22 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the KEY for the smallest value
 #if method is called and passed, should return nil
 
-def key_for_min_value(name_hash)
-  
+def key_for_min_value(pets)
+  pets = {:dog => 2, :cat => 2, :turtle => 1}
+  key_for_min_value(pets)
 
 end
