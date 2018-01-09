cacca09eee9f71432d5b4ea4ce0dd0f63ae67fb9
Helen Du <helendu.614@gmail.com>
2018-01-09 00:43:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,13 @@
 #if method is called and passed, should return nil
 
 def key_for_min_value(name_hash)
-  lowest_key = nil
-  lowest_value = nil
-
-  name_hash = {:dog => 2, :cat => 2, :turtle => 1}
-  key_for_min_value(name_hash)
-
+  method = nil
+  name_hash.each do |key, value|
+    if method == nil
+      method = key
+    elsif value < name_hash[method]
+      method = key
+    end
+  end
+  method
 end
