65448eaec22fac72629383f5c71e04c1a190a31b
Helen Du <helendu.614@gmail.com>
2018-01-09 00:37:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the KEY for the smallest value
+#if method is called and passed, should return nil
 
 def key_for_min_value(name_hash)
+  
 
 end
